292c1de0271d912eb3be1b8e96b777ec45bd99d8
HNUBIGroup <zy@htu.edu.cn>
2025-06-29 13:07:09 +0800

    init: first commit

--- independent.py
+++ independent.py
@@ -10,7 +10,7 @@ class Conv1d(nn.Module):
         p0 = int((kernel_size[0] - 1) / 2) if same_padding else 0
         self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=p0,
                               dilation=dilation, bias=True if if_bias else False)
-        self.bn = nn.BatchNorm1d(out_channels) if bn else None
+        self.bn = nn.BatchNorm1d(out_channels) if bn else None 
         self.relu = nn.ReLU(inplace=True) if relu else None
 
     def forward(self, x):
